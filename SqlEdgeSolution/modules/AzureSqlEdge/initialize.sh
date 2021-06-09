2cb25962296a5fcb7fefa316e1e7b3cbba2bf225
Marvin Garcia <magar@microsoft.com>
2021-06-09 19:50:07 -0400

    added bundled sql edge docker image

---
+++ SqlEdgeSolution/modules/AzureSqlEdge/initialize.sh
@@ -0,0 +1,5 @@
+# Wait for the SQL Server to come up
+sleep 90s
+
+# Run the setup script to create the DB and the schema in the DB
+/opt/mssql-tools/bin/sqlcmd -S localhost -U -P $MSSQL_SA_PASSWORD -i setup.sql
