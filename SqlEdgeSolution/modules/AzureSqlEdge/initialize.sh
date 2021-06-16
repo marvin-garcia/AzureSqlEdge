95a41e19bfa0aa5980ed1a1cf02a23025b0334f5
Marvin Garcia <magar@microsoft.com>
2021-06-16 14:08:52 -0400

    updates to sql edge image

--- SqlEdgeSolution/modules/AzureSqlEdge/initialize.sh
+++ SqlEdgeSolution/modules/AzureSqlEdge/initialize.sh
@@ -1,5 +1,5 @@
 # Wait for the SQL Server to come up
-sleep 90s
+sleep 120s
 
 # Run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -i setup.sql
